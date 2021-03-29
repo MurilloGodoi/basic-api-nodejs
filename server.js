edb023dd2565e97d5167931a007a7f34e183a96b
Murillo <murillogodoi13@hotmail.com>
2021-03-29 18:00:17 -0300

    Server logic changed

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const customExpress = require('./config/customExpress');
+const express = require("express");
+const routes = require("./routes");
+const connection = require("./Models/connection");
+const Tabelas = require("./Models/tabelas");
+connection.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    Tabelas.init(connection);
 
-const app = customExpress();
-
-app.listen(3000, () => console.log("Servidor Iniciado"));
+    const app = express();
+    app.use(express.urlencoded({ extended: true}))
+    app.use(express.json());
+    app.use(routes);
+    app.listen(3333);
+  }
+});
