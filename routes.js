7ee7dd8ac03b7d3f5b1c37a385e76a08f5277337
Murillo <murillogodoi13@hotmail.com>
2021-03-30 03:14:13 -0300

    add new routes

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
+const AtendimentosController = require('./controllers/AtendimentosController');
 const AtendimentoController = require('./controllers/AtendimentosController');
 
 const routes = Router();
 
-routes.get('/', AtendimentoController.index);
+routes.get('/atendimentos', AtendimentoController.index);
+routes.get('/atendimentos/:id', AtendimentosController.findOne)
 routes.post('/atendimentos', AtendimentoController.store);
+routes.patch('/atendimentos/update/:id', AtendimentoController.updateOne);
+routes.delete('/atendimentos/delete/:id', AtendimentoController.deleteById);
 
 module.exports = routes;
   
