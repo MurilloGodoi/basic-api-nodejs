1aa6053cf8d4268a574f6599d70ca1747336a074
Murillo <murillogodoi13@hotmail.com>
2021-03-30 01:29:26 -0300

    add date and date creation for new "Atendimentos"

--- Models/Atendimentos.js
+++ Models/Atendimentos.js
@@ -1,10 +1,15 @@
+const moment = require('moment');
 const connection = require("./connection");
 
 class Atendimento {
-  adicionaAtendimento(atendimento) {
+  adicionaAtendimento(dadosNovoAtendimento) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(dadosNovoAtendimento.data).format('YYYY-MM-DD HH:MM:SS');
+    const novoAtendimento = {...dadosNovoAtendimento, data, dataCriacao};
+
     const sql = "INSERT INTO Atendimentos SET ?";
 
-    connection.query(sql, atendimento, (erro) => {
+    connection.query(sql, novoAtendimento, (erro) => {
       if (erro) {
         console.log(erro);
       }
